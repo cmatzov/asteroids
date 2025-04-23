0863a347fcf237bf8c251b64d966d406d38ed8da
cmatzov <cristian.matzov@gmail.com>
2025-04-23 18:06:43 +0300

    player class, added to main player object that moves

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    drawables = pygame.sprite.Group()
+    updateables = pygame.sprite.Group()
+    
+    Player.containers = (drawables, updateables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updateables.update(dt)
+
         screen.fill("black")
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
