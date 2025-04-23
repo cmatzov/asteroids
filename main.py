5fe07377e240e43186eeb6bd267c842e13823232
cmatzov <cristian.matzov@gmail.com>
2025-04-23 16:39:11 +0300

    refactor main function, remove prints

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
